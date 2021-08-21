0a2a34e711a19bd516cb7e834d21d9af2c392cbf
P1SC3S03 <catarina-m-sousa@hotmail.com>
2021-08-21 11:04:29 +0100

    fixed functions -> now they render information in html

--- javascript.js
+++ javascript.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
     search = 'abba'.toLowerCase(); // CHANGE TO USER INPUT
     const searchEndPoint = 'https://www.theaudiodb.com/api/v1/json/1/search.php?s=';
-    fetch(searchEndPoint + search) 
+    fetch(searchEndPoint + search)
         .then(parseResponse)
-        .then(getImage)
-        .then(renderArtistBioPT)
-        .then(renderHome)
+        .then(artistInfo)
+        .then(renderImage)
+        //.then(renderArtistBioPT)
+        //.then(renderArtistBioEN)
         .catch(handleErrors);
 
 });
@@ -15,54 +16,45 @@ function parseResponse(response) {
     if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`)
     }
-    return response.json(); 
+    return response.json();
 }
-
-//VER HARD CODED ARRAY INDEX/POSITION!! 
 
 //ARTIST BIO IN PORTUGUESE
- function renderArtistBioPT(data) {
+function renderArtistBioPT(data) {
     let content = '<h3>Bio</h3>';
 
-    content += `<p>${data.artists[0].strBiographyPT}</p>`;
+    content += `<p>${data[0].bioPT}</p>`;
 
     $('#skills').html(content); //CHANGE SKILLS TO OTHER
- }
-
-//ARTIST BIO IN ENGLISH
- function renderArtistBioEN(data) {
-    let content = '<h3>Bio</h3>';
-
-    content += `<p>${data.artists[0].strBiographyEN}</p>`;
-
-    $('#skills').html(content); //CHANGE SKILLS TO OTHER
- }
-
-
-//TO CHECK IF SEARCH EXISTS AND IF SO RETURN POSITION
-function validateData(data) { 
-    let receivedArtistName = data.artists[0].strArtist.toLowerCase();
-    
-//FOR 
-    if(receivedArtistName !== search) {
-        throw new Error('Invalid artist');
-    }
-    return data;
 }
 
-//IMPORT IMAGE
-function getImage(data) {
-    //console.log(data, 'data!!');
-     console.log('data', data);
-     return data.artists[0].strArtistFanart;
- 
- }
+//ARTIST BIO IN ENGLISH
+function renderArtistBioEN(data) {
+    let content = '<h3>Bio</h3>';
 
-function renderHome(results) {
-    $('#home').html(`<div id="home-image"><img src="${results}"></div>
-    `)
+    content += `<p>${data[0].bioEN}</p>`;
+
+    $('#skills').html(content); //CHANGE SKILLS TO OTHER
+}
+
+//EXTRACT IMPORTANT ARTIST INFO
+function artistInfo(data) {
+    const info = data.artists.map(art => {
+        if (JSON.stringify(art.strArtist).toLowerCase() === JSON.stringify(search)) {
+            return {
+                name: art.strArtist, fanart: art.strArtistFanart, bioPT: art.strBiographyPT,
+                bioEN: art.strBiographyEN
+            }
+        } 
+    });
+    info.splice(1,1);
+    return info;
+}
+//RENDER IMAGE
+function renderImage(data) {
+    $('#home').html(`<div id="home-image"><img src="${data[0].fanart}"></div>`); //CHANGE HOME
 }
 
 function handleErrors(error) {
-    $('#home').html('<p style=“color: red;“>' + error + '</p>');
+    $('#home').html('<p style=“color: red;“>' + error + '</p>'); //CHANGE HOME
 }
