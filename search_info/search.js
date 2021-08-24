28e58afe0dde02c3c79a72535eba3851621009ce
miguelsilva110 <miguelsilvac220@gmail.com>
2021-08-24 10:39:26 +0100

    code cleaned and files not used deleted

--- search_info/search.js
+++ search_info/search.js
@@ -1,45 +1,66 @@
-const accessToken = 'BQBdLKBrWZCAVTFjoshnoOMzEQXkMrr8ysrDGqPxPU_zqUSKHlsLvNXW6bsWvA8R-wKwpsvgLlKq_FLKDPT1-r6_5IC5CX_kzkfmt6WS9VLTIAdU8sDx5sf3THPPlIz5n4qWPNA5ht5dtXy7uW4bH4OmsNVYsSOW0JfMONYVyCuMA33FN8hKewsrJJU';
+$(document).ready(function () {
 
-window.onload = () => {
-    const button = document.getElementById('button');
-
-    button.addEventListener('click', search);
-
-}
-
-function search() {
-    //Get the value of the search box
-    let raw_search_query = $('#search').val();
-    let search_query = encodeURI(raw_search_query);
-    // Make Spotify API call
-    $.ajax({
-        url: `https://api.spotify.com/v1/search?q=${search_query}&type=track,artist,album&limit=5`,
+    const getUrlParameter = (sParam) => {
+      let sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL != undefined && sPageURL.length > 0 ? sPageURL.split('#') : [],
+        sParameterName,
+        i;
+  
+      let split_str = window.location.href.length > 0 ? window.location.href.split('#') : [];
+      sURLVariables = split_str != undefined && split_str.length > 1 && split_str[1].length > 0 ? split_str[1].split('&') : [];
+  
+      for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] === sParam) {
+          return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+      }
+    };
+  
+    const accessToken = getUrlParameter('access_token');
+    let client_id = 'b1be58a2a84e423e88f88256823a1447';
+    let redirect_uri = 'http%3A%2F%2F127.0.0.1%3A5500%2Fdashboard.html';
+    window.history.pushState("","", 'dashboard.html');
+    const redirect = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}`;
+  
+    if (accessToken == null || accessToken == "" || accessToken == undefined) {
+      window.location.replace(redirect);
+    }
+  
+  
+    $("#search_button").click(function () {
+  
+      let raw_search_query = $('#search-text').val();
+      let search_query = encodeURI(raw_search_query);
+  
+      $.ajax({
+        url: `https://api.spotify.com/v1/search?q=${search_query}&type=track,artist,album&limit=3`,
         type: 'GET',
         headers: {
-            'Authorization': 'Bearer ' + accessToken
+          'Authorization': 'Bearer ' + accessToken
         },
         success: function (data) {
-            console.log(data)
-            // Load our songs from Spotify into our page
-            let num_of_tracks = data.tracks.items.length;
-            let count = 0;
-            // Max number of songs is 12
-            const max_songs = 12;
-
-            while (count < max_songs && count < num_of_tracks) {
-                // Extract the id of the FIRST song from the data object
-                let id = data.tracks.items[count].id;
-                // Constructing two different iframes to embed the song
-                let src_str = `https://open.spotify.com/embed/track/${id}`;
-                let iframe = `<div class='song'><iframe src=${src_str} frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe></div>`;
-                let parent_div = $('#song_' + count);
-                parent_div.html(iframe);
-                count++;
-            }
+          console.log(data)
+  
+          let num_of_tracks = data.tracks.items.length;
+          let count = 0;
+  
+          const max_songs = 12;
+          while (count < max_songs && count < num_of_tracks) {
+  
+            let id = data.tracks.items[count].id;
+  
+            let src_str = `https://open.spotify.com/embed/track/${id}`;
+            let iframe = `<div class='song'><iframe src=${src_str} frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe></div>`;
+            let parent_div = $('#song_' + count);
+            parent_div.html(iframe);
+            count++;
+          }
         }
-    }) // End of Spotify ajax call
-    .catch(error => console.error(error))
-} // End of search button */
+      });
+    });
+  });
+
 
 /*  function searchArtist() {
     const input = document.getElementById('search').value;
