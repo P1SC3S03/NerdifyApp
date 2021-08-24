46eb1f57cf1263091f83ed3b9cdaa8b7895d085b
P1SC3S03 <catarina-m-sousa@hotmail.com>
2021-08-24 11:43:24 +0100

    made audiodb 'approved' js file

---
+++ audiodb/audiodb.js
@@ -0,0 +1,130 @@
+let variable = '';
+
+$(document).ready(function () {
+    search = 'abba'.toLowerCase(); // CHANGE TO USER INPUT
+
+    //API KEY = 523532
+
+    const searchArtistUrl = 'https://www.theaudiodb.com/api/v1/json/1/search.php?s=';
+    const searchAlbumsByArtistURL = 'https://www.theaudiodb.com/api/v1/json/523532/searchalbum.php?s=';
+
+    fetch(searchArtistUrl + search)
+        .then(parseResponse)
+        .then(artistInfo)
+        .then(renderArtistHTML)
+        .catch(handleErrors);
+
+    fetch(searchAlbumsByArtistURL + search)
+        .then(parseResponse)
+        .then(fetchAlbunsByArtist)
+        .then(renderAlbumHTML)
+        .catch(handleErrors);
+
+});
+
+// PARSE JSON 
+function parseResponse(response) {
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`)
+    }
+    return response.json();
+}
+
+//EXTRACT IMPORTANT ARTIST INFO
+function artistInfo(data) {
+    const info = data.artists.map(artist => {
+        if (JSON.stringify(artist.strArtist).toLowerCase().includes(JSON.stringify(search))) {
+            return {
+                id: artist.idArtist, name: artist.strArtist, fanart: artist.strArtistFanart,
+                bioPT: artist.strBiographyPT, bioEN: artist.strBiographyEN,
+                country: artist.strCountry, countryInit: artist.strCountryCode,
+                genre: artist.strGenre, style: artist.strStyle
+            }
+        }
+    });
+    info.splice(1, 1);
+    variable = info;
+    return info;
+}
+
+//RENDER ARTIST HTML
+function renderArtistHTML(data) {
+    renderImage(data);
+    renderArtistBioPT(data);
+    renderArtistBioEN(data);
+    renderArtistCountry(data);
+    renderArtistGenreAndStyle(data);
+}
+
+//FETCH ALBUNS BY ARTIST
+function fetchAlbunsByArtist(data) {
+    console.log(data);
+
+    let info = data.album.map(album => {
+        if (!album.strDescriptionEN) {
+            album.strDescriptionEN = 'No data available';
+        }
+        if (!album.strDescriptionPT) {
+            album.strDescriptionPT = 'No data available';
+        }
+        return {
+            idAlbum: album.idAlbum, idArtist: album.idArtist,
+            albumName: album.strAlbum, releaseYear: album.intYearReleased,
+            descriptionEN: album.strDescriptionEN, descriptionPT: album.strDescriptionPT
+        }
+    });
+    console.log(info);
+    return info;
+}
+
+//HANDLE ERRORS
+function handleErrors(error) {
+    $('#home').html('<p style=“color: red;“>' + error + '</p>'); //CHANGE HOME
+}
+
+//------------------AUXILIAR METHODS--------------------//
+
+//RENDER IMAGE
+function renderImage(data) {
+    console.log(data);
+    $('#home').html(`<div id="home-image"><img src="${data[0].fanart}"></div>`); //CHANGE HOME
+}
+
+//ARTIST BIO IN PORTUGUESE
+function renderArtistBioPT(data) {
+    let content = '<h3>Bio</h3>';
+
+    content += `<p>${data[0].bioPT}</p>`;
+
+    $('#skills').html(content); //CHANGE SKILLS TO OTHER
+}
+
+//ARTIST BIO IN ENGLISH
+function renderArtistBioEN(data) {
+    let content = '<h3>Bio</h3>';
+
+    content += `<p>${data[0].bioEN}</p>`;
+
+    $('#skills').html(content); //CHANGE SKILLS TO OTHER
+}
+
+//ARTIST COUNTRY
+function renderArtistCountry(data) {
+    let content = '<h3>Country</h3>';
+
+    content += `<p>${data[0].country}: ${data[0].countryInit}</p>`;
+
+    $('#skills').html(content); //CHANGE SKILLS TO OTHER
+}
+
+//ARTIST GENRE AND STYLE
+function renderArtistGenreAndStyle(data) {
+    let content = '<h3>Genre And Style</h3>';
+
+    content += `<p><b>Genre:</b> ${data[0].genre} <br><b>Style:</b> ${data[0].style}</p>`;
+
+    $('#skills').html(content); //CHANGE SKILLS TO OTHER
+}
+
+
+
